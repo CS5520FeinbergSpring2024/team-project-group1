8ec028ab419fc46d0ad3a69e209310bd1202525b
Excalibur0331 <eli1838459978@gmail.com>
2024-03-20 11:46:18 -0700

    add forgot password UI

--- app/src/main/java/edu/northeastern/new_final/ui/LoginActivity.java
+++ app/src/main/java/edu/northeastern/new_final/ui/LoginActivity.java
@@ -2,15 +2,35 @@ package edu.northeastern.new_final.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import edu.northeastern.new_final.MainActivity; // Make sure this import matches the location of your MainActivity
 import edu.northeastern.new_final.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+    private Button buttonSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonSignup = findViewById(R.id.buttonSignUp);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
