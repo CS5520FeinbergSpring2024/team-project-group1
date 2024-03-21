00bae745db22720230b6d19633c3e7495683f632
spgallant <sarahpgallant@gmail.com>
2024-03-21 16:59:44 -0400

    updated nav bar with personal goal, challenge group, and header text & color

--- app/src/main/java/edu/northeastern/new_final/ui/challengeGroup/ChallengeGroupViewModel.java
+++ app/src/main/java/edu/northeastern/new_final/ui/challengeGroup/ChallengeGroupViewModel.java
@@ -1,16 +1,16 @@
-package edu.northeastern.new_final.ui.gallery;
+package edu.northeastern.new_final.ui.challengeGroup;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class ChallengeGroupViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public ChallengeGroupViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is the challenge group fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/edu/northeastern/new_final/ui/personalGoal/PersonalGoalFragment.java
+++ app/src/main/java/edu/northeastern/new_final/ui/personalGoal/PersonalGoalFragment.java
@@ -1,4 +1,4 @@
-package edu.northeastern.new_final.ui.slideshow;
+package edu.northeastern.new_final.ui.personalGoal;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import edu.northeastern.new_final.databinding.FragmentSlideshowBinding;
 
-public class SlideshowFragment extends Fragment {
+import edu.northeastern.new_final.databinding.FragmentPersonalGoalBinding;
 
-    private FragmentSlideshowBinding binding;
+public class PersonalGoalFragment extends Fragment {
+
+    private FragmentPersonalGoalBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+        PersonalGoalViewModel personalGoalViewModel =
+                new ViewModelProvider(this).get(PersonalGoalViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentPersonalGoalBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textPersonalGoal;
+        personalGoalViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
