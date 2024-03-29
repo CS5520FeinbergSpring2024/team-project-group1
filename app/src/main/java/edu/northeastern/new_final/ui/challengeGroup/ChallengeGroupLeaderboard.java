76bdc6f3bbed1a238b07bf61d1b71b4cc9efba21
rehmaund <rehmaund44@gmail.com>
2024-03-29 17:24:13 -0400

    Add activities for new group pages

---
+++ app/src/main/java/edu/northeastern/new_final/ui/challengeGroup/ChallengeGroupLeaderboard.java
@@ -0,0 +1,15 @@
+package edu.northeastern.new_final.ui.challengeGroup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.new_final.R;
+
+public class ChallengeGroupLeaderboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.challenge_group_leaderboard);
+    }
+}
---
+++ app/src/main/java/edu/northeastern/new_final/ui/challengeGroup/CreateChallengeGroup.java
@@ -0,0 +1,15 @@
+package edu.northeastern.new_final.ui.challengeGroup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.new_final.R;
+
+public class CreateChallengeGroup extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_group);
+    }
+}
