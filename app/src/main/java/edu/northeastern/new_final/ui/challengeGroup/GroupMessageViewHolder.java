96d54527a971e8d5b990eecc75af623dbf07a095
rehmaund <rehmaund44@gmail.com>
2024-04-17 14:23:20 -0400

    Create adapter and view holder for recycler view of messages on Group Messages page

---
+++ app/src/main/java/edu/northeastern/new_final/ui/challengeGroup/GroupMessageViewHolder.java
@@ -0,0 +1,33 @@
+package edu.northeastern.new_final.ui.challengeGroup;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import edu.northeastern.new_final.R;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import javax.annotation.Nonnull;
+
+public class GroupMessageViewHolder extends RecyclerView.ViewHolder {
+
+
+    TextView username;
+
+    TextView date;
+    TextView message;
+
+    public GroupMessageViewHolder(@Nonnull View itemView) {
+        super(itemView);
+
+        username = itemView.findViewById(R.id.usernameTextView);
+        date = itemView.findViewById(R.id.dateTextView);
+        message = itemView.findViewById(R.id.messageTextView);
+    }
+
+    public void bind(GroupMessage groupMessage) {
+        username.setText(groupMessage.getUsername());
+        date.setText(groupMessage.getDate());
+        message.setText(groupMessage.getMessage());
+    }
+}
